e30e282ac1983f10b8da953217c404420b4551fa
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2026-01-13 09:29:57 +0100

    feat(base): auth layout

---
+++ base/app/components/layout/LayoutAuth.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineSlots<{
+  default: any
+}>()
+</script>
+
+<template>
+  <div class="flex min-h-full flex-col items-center justify-center gap-4 p-4">
+    <UPageCard class="w-full max-w-md">
+      <slot />
+    </UPageCard>
+  </div>
+</template>
