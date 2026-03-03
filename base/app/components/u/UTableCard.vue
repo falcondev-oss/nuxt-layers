eb0c83abf87261e47285f459aaf0cc13e428097e
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2026-03-03 07:50:15 +0100

    feat(base): forward `UTableCard` props

--- base/app/components/u/UTableCard.vue
+++ base/app/components/u/UTableCard.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-import type { CardSlots } from '@nuxt/ui'
+import type { CardProps, CardSlots } from '@nuxt/ui'
+import { useForwardProps } from 'reka-ui'
+const props = defineProps<CardProps>()
 const slots = defineSlots<CardSlots>()
+
+const forwarded = useForwardProps(props)
 </script>
 
 <template>
   <UCard
+    v-bind="forwarded"
     :ui="{
       body: 'p-0!',
+      ...forwarded.ui,
     }"
   >
     <template v-for="(_, name) of slots" #[name]>
